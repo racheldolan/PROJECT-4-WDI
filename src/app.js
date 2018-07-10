adfbbbd32f09d7d638bf24f3689aac0b7764e93c
Rachel Dolan <racheldolan1@gmail.com>
2018-07-10 20:48:01 +0100

    login component and route working

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import 'bulma';
 
 import AuthRegister from './components/auth/Register';
+import AuthLogin from './components/auth/Login';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Route path="/register" component={AuthRegister} />
+        <Switch>
+          <Route path="/register" component={AuthRegister} />
+          <Route path="/login" component={AuthLogin} />
+        </Switch>
       </BrowserRouter>
     );
   }
--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,8 +1,45 @@
 import React from 'react';
 import axios from 'axios';
 
+import Auth from '../../lib/Auth';
+
 class AuthLogin extends React.Component {
 
+  state = {}
+
+  handleChange = ({ target: { name, value }}) => {
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    axios({
+      url: '/api/login',
+      method: 'POST',
+      data: this.state
+    })
+      .then(res => {
+        Auth.setToken(res.data.token);
+        this.props.history.push('/');
+      });
+  }
+
+  render(){
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <div className="field">
+          <label className="email">Email</label>
+          <input className="input" name="email" placeholder="Email" onChange={this.handleChange} />
+        </div>
+        <div className="field">
+          <label className="password">Password</label>
+          <input className="input" type="password" name="password" placeholder="Password" onChange={this.handleChange}/>
+        </div>
+
+        <button className="button">Submit</button>
+      </form>
+    );
+  }
 }
 
 export default AuthLogin;
