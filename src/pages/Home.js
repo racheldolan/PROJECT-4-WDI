d73e0ced082f160d39005bf46fdf3a8e4869d61c
Rachel Dolan <racheldolan1@gmail.com>
2018-08-01 11:38:27 +0100

    added condition to check if a user is logged in and hides/displays the 'login' button on homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Auth from '../lib/Auth';
 
 const Home = () => {
   return(
@@ -6,7 +7,7 @@ const Home = () => {
       <h1 className="home-title">Chapter</h1>
       <h2 className="home-subtitle">Expand your library and your friendships</h2>
       <a href="/groups" className="button home-button">Browse Groups</a>
-      <a href="/login" className="button home-button">Login</a>
+      {!Auth.isAuthenticated() && <a href="/login" className="button home-button">Login</a>}
     </main>
   );
 };
